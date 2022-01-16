0336ab840ea6804ea0132505d9a45c96fca07ba4
Prakrity Srivastava <83003431+prakritisrivastava@users.noreply.github.com>
2022-01-16 14:08:28 +0530

    Add files via upload

---
+++ The-National-Bank-master/README.md
@@ -0,0 +1,3 @@
+# Basic Banking System
+Basic banking system website using html css bootstrap for front end, Php for backend and phpmyadmin sql for database.
+
---
+++ The-National-Bank-master/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>The National Bank</title>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- Navigation bar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">The National Bank</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="customers.php">Customers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="transactions.php">Transfer History</a>
+        </li>
+         
+        
+</div>
+
+
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+<!-- Caraousel-->
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/bg3.jpg" alt="Easy Transfer" width="1100" height="1000">
+      <div class="carousel-caption">
+    <h3>Easy Money Transfer </h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/bank1.jpg" alt="Quick Deposit" width="1100" height="1000">
+      <div class="carousel-caption  d-none d-md-block">
+    <h3>Quick Personal Loans</h3>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/cus.jpg" alt="customer satisfaction" width="1100" height="1000">
+      <div class="carousel-caption">
+    <h3>Customer Satisfaction</h3>
+    </div>
+    </div>
+  </div>
+  
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<!--About us-->
+
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center" >About Us</h3>
+ </div>
+
+<div class="container-fluid">
+ <div class ="row">
+<div  class="col-lg-6 col-md-6 col-12">
+      <img src="img/vdo3.gif" class="img-fluid">
+      </div>
+      <div  class="col-lg-6 col-md-6 col-12">
+      <h1>The National Bank</h1>
+      <p1> The National Bank is a part of The Sparks foundation. It is one of nations's leading private banks and was among the first to receive approval from the Central bank of nation.
+          Today, The National Bank has a banking network of 2,274 branches and 15,087 ATM's in 1,900 cities/towns. The National Bank offers a diverse range of financial products and banking services to customers through a growing branch and ATM network and digital channels such as Netbanking, Phonebanking and MobileBanking.
+        </div>  
+         
+
+ </div>
+ </div>
+
+<!--Dsiclaimer-->
+
+</section>
+<section class= "my-5">
+<footer class="text-center mt-2 py-5">
+        <p>&copy 2021  <b>Prakrity Srivastava</b> </br>CEO, The National Bank</p>
+      </footer>
+</section>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>  
+</body>
+</html>
